ff1738c831271623f50c04c63c140e6a9c9794c5
sunao <346348151@qq.com>
2023-12-24 12:20:25 +0800

    两数相加

---
+++ src/main/java/leetcode/editor/cn/两数相加2.java
@@ -0,0 +1,102 @@
+  //给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。 
+//
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。 
+//
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：l1 = [2,4,3], l2 = [5,6,4]
+//输出：[7,0,8]
+//解释：342 + 465 = 807.
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [0], l2 = [0]
+//输出：[0]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//输出：[8,9,9,9,0,0,0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 每个链表中的节点数在范围 [1, 100] 内 
+// 0 <= Node.val <= 9 
+// 题目数据保证列表表示的数字不含前导零 
+// 
+//
+// Related Topics递归 | 链表 | 数学 
+//
+// 👍 10141, 👎 0bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+  
+  package leetcode.editor.cn;
+  
+  /**
+   * 2:两数相加
+   */
+  public class 两数相加2 {
+      public static void main(String[] args) {
+           Solution solution = new 两数相加2().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 双指针
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        // 进位
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            // 先把进位加上
+            int val = carry;
+            if (l1 != null) {
+                val = val + l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                val = val+l2.val;
+                l2 = l2.next;
+            }
+            // 处理进位
+            carry = val / 10;
+            // 处理此位的值
+            val = val % 10;
+            ListNode node = new ListNode(val);
+            curr.next = node;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
